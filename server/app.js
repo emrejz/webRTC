38be63f41e53bd17365c4e4ecccab69dc7b86501
emre kayacan <emre.kayacan@msn.com>
2020-08-24 03:55:53 +0300

    socket ok

--- server/app.js
+++ server/app.js
@@ -5,13 +5,31 @@ const server = http.createServer(app);
 const socket = require("socket.io");
 const io = socket(server);
 
+let users = [];
+
 io.on("connection", (socket) => {
-  console.log(socket.id);
+  users.push(socket.id);
+
+  socket.emit("all users", users);
+
+  socket.on("sending signal", (payload) => {
+    socket.broadcast.emit("user joined", {
+      signal: payload.signal,
+      callerID: payload.callerID,
+    });
+  });
+
+  socket.on("returning signal", (payload) => {
+    socket.broadcast.emit("receiving returned signal", {
+      signal: payload.signal,
+      id: socket.id,
+    });
+  });
+
   socket.on("disconnect", () => {
-    console.log("disconnect");
+    users = users.filter((id) => id != socket.id);
   });
 });
-
 server.listen(process.env.PORT || 3001, () =>
   console.log("server is running on port 3001")
 );
