956e1aa24b21f5d2435e79d0fa2f66cc5b268f96
emre kayacan <emre.kayacan@msn.com>
2020-08-24 03:58:36 +0300

    simple-peer and socket ok but have some bugs

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,20 +1,122 @@
 import React, { useEffect, useRef, useState } from "react";
+import io from "socket.io-client";
+import Peer from "simple-peer";
+import CustomVideo from "./CustomVideo";
 
 import "./index.css";
 
 const Index = () => {
+  const [peers, setPeers] = useState([]);
+  const [error, setError] = useState("");
+  const socketRef = useRef();
   const userVideo = useRef();
+  const peersRef = useRef([]);
+
   useEffect(() => {
     navigator.mediaDevices
       .getUserMedia({ video: true, audio: true })
       .then((stream) => {
+        socketRef.current = io.connect("http://localhost:3001/");
+
         userVideo.current.srcObject = stream;
-        console.log(stream);
+
+        socketRef.current.on("all users", (users) => {
+          const peers = [];
+          users.forEach((userID) => {
+            const peer = createPeer(userID, socketRef.current.id, stream);
+            peersRef.current.push({
+              peerID: userID,
+              peer,
+            });
+            userID != socketRef.current.id && peers.push(peer);
+          });
+          setPeers(peers);
+        });
+
+        socketRef.current.on("user joined", (payload) => {
+          const item = peersRef.current.find(
+            (p) => p.peerID === payload.callerID
+          );
+
+          if (!item) {
+            const peer = addPeer(payload.signal, payload.callerID, stream);
+            peersRef.current.push({
+              peerID: payload.callerID,
+              peer,
+            });
+
+            setPeers((users) => [...users, peer]);
+          }
+        });
+
+        socketRef.current.on("receiving returned signal", (payload) => {
+          const item = peersRef.current.find((p) => p.peerID === payload.id);
+          item.peer.signal(payload.signal);
+        });
+      })
+      .catch((err) => {
+        setError(err);
+        console.log(err);
       });
   }, []);
+
+  function createPeer(userToSignal, callerID, stream) {
+    const peer = new Peer({
+      initiator: true,
+      trickle: false,
+      stream,
+    });
+    peer.on("error", (err) => {
+      console.log(err);
+      setError(err);
+    });
+    peer.on("close", () => {
+      setPeers(peers.filter((item) => item.peer != peer));
+    });
+    peer.on("signal", (signal) => {
+      socketRef.current.emit("sending signal", {
+        userToSignal,
+        callerID,
+        signal,
+      });
+    });
+
+    return peer;
+  }
+
+  function addPeer(incomingSignal, callerID, stream) {
+    const peer = new Peer({
+      initiator: false,
+      trickle: false,
+      stream,
+    });
+
+    peer.on("error", (err) => {
+      console.log(err);
+      setError(err);
+    });
+    peer.on("close", () => {
+      setPeers(peers.filter((item) => item.peer != peer));
+    });
+    peer.on("signal", (signal) => {
+      socketRef.current.emit("returning signal", { signal, callerID });
+    });
+
+    peer.signal(incomingSignal);
+    return peer;
+  }
   return (
-    <div className="videosContainer">
-      <video className="userVideo" muted ref={userVideo} autoPlay playsInline />
+    <div className="homeContainer">
+      <div className="videosContainer">
+        <video muted autoPlay playsInline ref={userVideo}></video>
+        {peers.map((peer, index) => {
+          return <CustomVideo key={index} peer={peer} />;
+        })}
+      </div>
+      {peers.length == 0 && (
+        <h1>At least 1 more person should come to the conference</h1>
+      )}
+      {error && error.message && <h1>{error.message}</h1>}
     </div>
   );
 };
